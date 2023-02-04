649dd269190511fccaab0112c287b4aba3777e1f
yinglin <yingtan114@gmail.com>
2023-02-04 15:41:41 -0800

    Added headers and updated log file

--- WS1/1.17LAB.c
+++ WS1/1.17LAB.c
@@ -1,3 +1,11 @@
+/**
+ * Description: This lab is an exercise on printing an arrow based on input values.
+ * Author name: Yinglin Tan
+ * Author email: yinglin.tan@sjsu.edu
+ * Last modified date: 2/4/2023
+ * Creation date: 2/4/2023
+ **/
+
 #include <stdio.h>
 
 int main(void) {
