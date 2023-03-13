3219a44b27ba268b9792f77611d51d4c09d8533f
yinglin <yingtan114@gmail.com>
2023-03-12 22:19:01 -0700

    Update current code

--- A3/proc_manager.c
+++ A3/proc_manager.c
@@ -10,20 +10,25 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define MAX_CHAR 100
 #define MAX_LEN 30
 
 
 int main(int argc, char *argv[]){
-    //If user does not give exactly 1 input, print warning and exit as 2
-    if (argc != 2){
-        printf("One input is needed.\n");
-        exit(2);
-    }
+    bool file_exist = 0;
+    FILE* cmd_fp; //declare file pointer
 
-    //declare file pointer
-    File* cmd_fp = fopen(argv[1], "r");
+    //If user does not give exactly 1 input, print warning and exit as 2
+    if (argc == 2){
+        cmd_fp  = fopen(argv[1], "r");
+        file_exist = 1;
+    }
 
     //if file doesn't exist, print error message and exit as 1
     if (cmd_fp == NULL){
@@ -42,21 +47,29 @@ int main(int argc, char *argv[]){
         fseek(cmd_fp, 0, SEEK_SET); //go back to top of file
     }
 
+    int status; //declare status
+    pid_t child; //declare child pid
+    char* loop;
+    //char commands[MAX_LEN] = {0}; //array to store commands
+    char current_line[MAX_LEN]; //array to store current line read
+    int line_count = 0; //counter for line count
+    int cmd_count = 0;
 
-    while(fgets(current_line, MAX_LEN, cmd_fp) != -1 && line_count < MAX_CHAR){
-        //char commands[MAX_LEN] = {0}; //array to store commands
-        char current_line[MAX_LEN]; //array to store current line read
-        int line_count = 0; //counter for line count
-        int cmd_count = 0;
+    if(file_exist){
+        loop = fgets(current_line, MAX_LEN, cmd_fp);
+    }
+    else{
+        loop = fgets(current_line, MAX_LEN, stdin);
+    }
 
+
+    while(loop != NULL && line_count < MAX_CHAR){
         //convert to C string
         if (current_line[strlen(current_line) - 1] == '\n'){
             current_line[strlen(current_line) - 1] = '\0';
         }
 
         cmd_count++;
-
-        pid_t child; //declare child pid
 
         child = fork();
 
@@ -68,7 +81,7 @@ int main(int argc, char *argv[]){
         //child process
         else if (child == 0){
             //split current line into parts by words
-            char *argument[MAX_LEN + 1];
+            char *argument[MAX_LEN + 1] = {0};
             char *word = strtok(current_line, " ");
             int counter = 0;
 
@@ -80,7 +93,11 @@ int main(int argc, char *argv[]){
             }
             argument[counter] = NULL;
 
-            printf("Starting command %d: child %d pid of parent %d\n", cmd_count, getpid(), getppid());
+            for(int k = 0; k < 10;k++){
+                printf("in argument: %s\n", argument[k]);
+            }
+
+            fprintf(stdout,"Starting command %d: child %d pid of parent %d\n", cmd_count, getpid(), getppid());
 
             char output_file[MAX_LEN]; // array to hold stdout
             char error_file[MAX_LEN]; // array to hold stderr
@@ -97,52 +114,69 @@ int main(int argc, char *argv[]){
             dup2(fd_1, 1);
             dup2(fd_2, 2);
 
-            execvp();
+            printf("log file done\n");
+            fflush(stdout);
+
+            //check if execvp ran properly
+            if(execvp(argument[0], argument) == -1){
+                perror(argument[0]);
+                exit(2);
+            }
+
         }
 
+        //parent process
         while((child = wait(&status)) > 0){
-            char output_file[30]; // array to hold stdout
-            char error_file[30]; // array to hold stderr
+            printf("start of parent\n");
+
+            char output_file[MAX_LEN] = {0}; // array to hold stdout
+            char error_file[MAX_LEN] = {0}; // array to hold stderr
 
             //push the logs to their respective files
-            sprintf(output_file, "%d.out", (int) getpid());
-            sprintf(error_file, "%d.err", (int) getpid());
+            sprintf(output_file, "%d.out", child);
+            sprintf(error_file, "%d.err", child);
+
+            printf("sprintf done\n");
 
             //open log files
             int fd_1 = open(output_file, O_RDWR | O_CREAT | O_APPEND, 0777);
-            int fd_2 = open(error_file, O_RDWR | O_CREAT | O_APPEND, 0777);
+            dup2(fd_1, 1);
 
-            fprintf(stdout, "Finished child %d pid of parent %d\n", child, (int) getpid());
-            //fflush(stdout);
+            fflush(stdout);
+
+            int fd_2 = open(error_file, O_RDWR | O_CREAT | O_APPEND, 0777);
+            dup2(fd_2, 2);
+
+            printf("open log files done\n");
+
+            //send fd_1 to PID.out file and fd_2 to PID.err for the PID
+
+            printf("parent: log file done\n");
 
             //if process exited normally
             if (WIFEXITED(status)) {
+                fprintf(stdout, "Finished child %d pid of parent %d\n", child, (int) getpid());
+                fflush(stdout); //clear stdout
                 fprintf(stderr, "Exited with exitcode = %d\n", WEXITSTATUS(status));
             }
             //if process killed
             else if (WIFSIGNALED(status)) {
                 fprintf(stderr, "Killed with signal %d\n", WTERMSIG(status));
             }
-
-            //send fd_1 to PID.out file and fd_2 to PID.err for the PID
-            dup2(fd_1, 1);
-            dup2(fd_2, 2);
+            fclose(cmd_fp);
+/*
+            if(current_line){
+                free(current_line);
+            }*/
         }
+        line_count++;
+
     }
 
 
-
     //notes:
-    //call execvp in child process
-    //push results to output file and errors to error file
-    //figure out what to do in parent process
-    //how many parent processes are needed?
-    //should we read in the entire input into array and then call child processes
-    //or should we go line by line and call child processes for every line?
-    //should we go line by line after while or inside the while?
-    //how to check if file is txt file or actual intput?
-    //pass current line to buffer?
-    //how to separate string into parts? (need to google)
+    //how to print things in the correct order?
+    //how to keep the loop going until last command?
 
 
     return 0;
