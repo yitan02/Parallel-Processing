5660387daf959e19e98e02d744be2bfa3fb7cf71
yinglin <yingtan114@gmail.com>
2023-03-26 21:51:58 -0700

    Finished WS8

--- WS8/6.27Lab.c
+++ WS8/6.27Lab.c
@@ -1,5 +1,5 @@
 /**
- * Description: This program count how many times a letter appeared in a word
+ * Description: This program count how many times a character appeared in a word
  * Author names: Talia Syed, Yinglin Tan
  * Author emails: talia.syed@sjsu.edu, yinglin.tan@sjsu.edu
  * Last modified date: 3/26/23
@@ -10,58 +10,61 @@
 #include <string.h>
 
 /**
- * Calculate how many times a letter appeared in a word
+ * Calculate how many times a character appeared in a word
  * Assumptions: a string and char is passed
  * Input: a string of less than 50 char and a search char
  * Output: int
  **/
 int CalcNumCharacters(char* userString, char userChar){
-   int count = 0; //counter for userString
+   int counter = 0;
 
-   //traverse userString to find if current character and key match
-   while (*userString != '\0'){
-      //if key character and string character match, incrment counter
-      if(*userString == userChar){
-         count++;
-      }
-      *userString++; //increment pointer
+   //for loop checks if userString contains userChar
+   for(int j = 0; j < strlen(userString); j++){
+         //increase counter if userChar found
+         if(userString[j] == userChar){
+            counter++;
+         }
    }
-   return count;
+
+   //return counter
+   return counter;
 }
 
 /**
  * Gets user input, calls CalcNumCharacters, and print out the result
- * Assumptions: user inputs a string less than 50 characters
+ * Assumptions: user inputs a string less than 52 characters (including first character and space)
  * Input: void
  * Output: int
  **/
 int main(void) {
-   char input[52] = {0}; //take input from stdin
-   char input_char; // hold the input character
-   char input_string[50] = {0}; // hold the input string
-   int counter = 0; //counter for adding input to string array
-   char *ptr = input_string; //pointer for string array
+   const int string_size = 50; //size of string
+   const int input_size = 52; //size of input including the searchChar and space
+   char input_string[input_size];  //array to hold the entire line of input
+   char search_string[string_size]; //array to hold the string after searchChar
 
-   fgets(input, sizeof(input), stdin); // take in entire input
+   //scan input to input_string
+   fgets(input_string, input_size, stdin);
 
-   input_char = input[0]; //first character of input is comparison character
+   //set searchChar to the first letter of input_String
+   char searchChar = input_string[0];
 
-   //add the input from stdin to the string array
-   for(int i = 2; input_string[i] != '\0'; i++){
-      input_string[counter] = input[i];
-      counter++;
+   //put the string after the searchChar to search_string
+   int index = 0;
+   for(int i = 2; i < strlen(input_string); i++){
+         search_string[index] = input_string[i];
+         index++;
    }
 
-   int numCalc = CalcNumCharacters(ptr, input_char); // run funtion and put result into an integer variable
+   //call function and assign returned int to the variable return
+   int result = CalcNumCharacters(search_string, searchChar);
 
-   //if the output is not one, then print in plural form
-   if(numCalc != 1){
-      printf("%d %c's\n", numCalc, input_char);
+   //print plural form if result is not equal to 1
+   if(result != 1){
+      printf("%d %c's\n", result, searchChar);
    }
-   //if output is one, print in singular form
-   else {
-      printf("%d %c\n", numCalc, input_char);
+   //print singular form if equal to 1
+   else{
+      printf("%d %c\n", result, searchChar);
    }
-
    return 0;
 }
